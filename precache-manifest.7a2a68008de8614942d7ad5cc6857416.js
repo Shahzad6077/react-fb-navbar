c896fc90383928481a83840a90f4468bc34a6008
shahzad6077 <alim6077@gmail.com>
2020-04-29 07:54:37 +0500

    Updates

---
+++ precache-manifest.7a2a68008de8614942d7ad5cc6857416.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669ee36a71e8c6dec7200d93c70f7ce1",
+    "url": "/react-fb-navbar/index.html"
+  },
+  {
+    "revision": "e46e368e7f0a53bbbe83",
+    "url": "/react-fb-navbar/static/css/main.444cefb9.chunk.css"
+  },
+  {
+    "revision": "0bb707fdfa27137feaf8",
+    "url": "/react-fb-navbar/static/js/2.2fcf967f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-fb-navbar/static/js/2.2fcf967f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46e368e7f0a53bbbe83",
+    "url": "/react-fb-navbar/static/js/main.a4f97107.chunk.js"
+  },
+  {
+    "revision": "35da86a6c8f525eff352",
+    "url": "/react-fb-navbar/static/js/runtime-main.4f1a4e23.js"
+  },
+  {
+    "revision": "c7552ce8c1730b613d60739e019be83c",
+    "url": "/react-fb-navbar/static/media/chevron-left.c7552ce8.svg"
+  },
+  {
+    "revision": "fa19fd039a8a44f11782bf26a330aab6",
+    "url": "/react-fb-navbar/static/media/chevron-right.fa19fd03.svg"
+  },
+  {
+    "revision": "0868da60be884ce93a53fd25bfd86272",
+    "url": "/react-fb-navbar/static/media/cog.0868da60.svg"
+  },
+  {
+    "revision": "ca82bc743710e8f84904d035d92bf5cd",
+    "url": "/react-fb-navbar/static/media/down-arrow.ca82bc74.svg"
+  },
+  {
+    "revision": "191410cf3b05978012dc7de226c0e4db",
+    "url": "/react-fb-navbar/static/media/messenger.191410cf.svg"
+  },
+  {
+    "revision": "9c07e9803bc685f8f9c96a9427dce9c0",
+    "url": "/react-fb-navbar/static/media/notification.9c07e980.svg"
+  },
+  {
+    "revision": "d0f5911d9a71bc7ec61eff8a4f25ea4e",
+    "url": "/react-fb-navbar/static/media/plus-i.d0f5911d.svg"
+  }
+]);
